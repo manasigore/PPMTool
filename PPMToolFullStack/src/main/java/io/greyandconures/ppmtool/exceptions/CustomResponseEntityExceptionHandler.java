f60455b800e79c74fd233b7c1cca87242613fc46
Manasi Gore <manasi.gore490@gmail.com>
2019-10-19 20:35:35 -0400

    find projectByIdentifier in the project object

--- PPMToolFullStack/src/main/java/io/greyandconures/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
+++ PPMToolFullStack/src/main/java/io/greyandconures/ppmtool/exceptions/CustomResponseEntityExceptionHandler.java
@@ -4,6 +4,7 @@ package io.greyandconures.ppmtool.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -11,6 +12,8 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 @RestController
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
     public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request) {
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
 
