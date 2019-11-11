fbbf333b4306c94f277de52eb0dba6630b46b506
Manasi Gore <manasi.gore490@gmail.com>
2019-11-10 19:20:44 -0500

    connected redux

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -5,16 +5,20 @@ import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import AddProject from './components/Project/AddProject';
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
