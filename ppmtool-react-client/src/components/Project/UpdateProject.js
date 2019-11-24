38ab1b5031c7d286bcfa84d5eba6142711fe62ec
Manasi Gore <manasi.gore490@gmail.com>
2019-11-24 13:51:03 -0500

    added error validation

--- ppmtool-react-client/src/components/Project/UpdateProject.js
+++ ppmtool-react-client/src/components/Project/UpdateProject.js
@@ -15,7 +15,8 @@ class UpdateProject extends Component {
             projectIdentifier: "",
             description: "",
             start_date: null,
-            end_date: null
+            end_date: null,
+            errors: {}
         };
     }
 
@@ -36,7 +37,13 @@ class UpdateProject extends Component {
             description,
             start_date,
             end_date,
-        })
+        });
+
+        if (nextProps.errors) {
+            this.setState({
+                errors: nextProps.errors,
+            });
+        }
     }
 
     componentDidMount() {
@@ -63,6 +70,7 @@ class UpdateProject extends Component {
     }
 
     render() {
+        const { errors } = this.state;
         return (
             <div className="project">
                 <div className="container">
@@ -74,12 +82,17 @@ class UpdateProject extends Component {
                                 <div className="form-group">
                                     <input
                                         type="text"
-                                        className="form-control form-control-lg "
+                                        className={classnames("form-control form-control-lg ", {
+                                            "is-invalid": errors.projectName
+                                        })}
                                         placeholder="Project Name"
                                         name="projectName"
                                         value={this.state.projectName}
                                         onChange={this.onChange}
                                     />
+                                    {errors.projectName && (
+                                        <div className="invalid-feedback">{errors.projectName}</div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <input
@@ -94,12 +107,17 @@ class UpdateProject extends Component {
                                 </div>
                                 <div className="form-group">
                                     <textarea
-                                        className="form-control form-control-lg"
+                                        className={classnames("form-control form-control-lg", {
+                                            "is-invalid": errors.description
+                                        })}
                                         placeholder="Project Description"
                                         name="description"
                                         value={this.state.description}
                                         onChange={this.onChange}
                                     />
+                                    {errors.projectName && (
+                                        <div className="invalid-feedback">{errors.description}</div>
+                                    )}
                                 </div>
                                 <h6>Start Date</h6>
                                 <div className="form-group">
@@ -139,9 +157,11 @@ UpdateProject.propTypes = {
     getProject: PropTypes.func.isRequired,
     project: PropTypes.object.isRequired,
     createProject: PropTypes.func.isRequired,
+    errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
+    errors: state.error,
     project: state.project.project,
 });
 
